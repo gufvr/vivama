c88221241adc05a2e036f3f345a969d9d9a90871
gufvr <gu.fvrr@gmail.com>
2025-09-18 23:44:31 -0300

    feat: add header layout with patient list icon
    - Created `App` component structure in `index.js`
    - Implemented header with title "VivaMa!" and patient list icon
    - Added basic styling for container, header, and icon
    - Updated `package.json` with `expo-module-scripts` dev dependency

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Ionicons } from "@expo/vector-icons";
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <View style={styles.header}>
+        <Text style={styles.headerText}>VivaMa!</Text>
+
+        <View style={styles.patientListView}>
+          <View style={styles.iconContainer}>
+            <Ionicons name="people-sharp" size={24} color="black" />
+          </View>
+        </View>
+      </View>
+      
+      <View>
+        <View>
+
+        </View>
+      </View>
     </View>
   );
 }
@@ -16,5 +33,34 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    width: "100%",
+    paddingHorizontal: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 10,
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "black",
+  },
+  patientListView: {
+    flex: 1,
+    flexDirection: "row",
+    borderRadius: 8,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  iconContainer: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
